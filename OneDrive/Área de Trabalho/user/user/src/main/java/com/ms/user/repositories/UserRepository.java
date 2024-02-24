5c34287f660e9f806d0543e33d7c07c4fe150dee
LOUDE-DJEMA SIME <105392673+loudedje@users.noreply.github.com>
2024-02-24 19:24:28 -0300

    Delete OneDrive/Área de Trabalho/user directory

--- OneDrive/Área de Trabalho/user/user/src/main/java/com/ms/user/repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ms.user.repositories;
-import com.ms.user.model.UserModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserModel, Long> {
-
-}
